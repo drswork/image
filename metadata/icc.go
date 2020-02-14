3ea7c77522b9ddbeb86534795711cab5acfa1859
drswork <dsugalski@google.com>
2020-02-13 19:54:10 -0500

    Add a few ICC base types
    The ICC standard clearly defines a bunch of base types that it uses which don't directly correspond to Go's standard types. Define them so we can use them later.
    
    This is necessary to avoid potential conversion loss between Go types and ICC types, even if it's a bit annoying to deal with. (For example the ICC defines several fixed-point decimal types)

--- metadata/icc.go
+++ metadata/icc.go
@@ -20,10 +20,59 @@ func RegisterICCEncoder(e func(context.Context, *ICC, ...image.WriteOption) ([]b
 	iccEncoder = e
 }
 
+// Some basic ICC types.
+
+// S15Fixed16 holds a signed 32 bit fixed point number, with 1 sign
+// bit, 15 integer bits, and 16 fractional bits.
+type S15Fixed16 struct {
+	Integer  int16
+	Fraction uint16
+}
+
+// U16Fixed16 holds an unsigned 32 bit fixed point number with 16
+// integer bits and 16 fractional bits.
+type U16Fixed16 struct {
+	Integer  uint16
+	Fraction uint16
+}
+
+// U8Fixed8 holds an unsigned 16 bit fixed point number, with 8
+// integer bits and 8 fraction bits.
+type U8Fixed8 struct {
+	// Integer holds the unsigned 8 bit integer portion of the number.
+	Integer uint8
+	// Fraction holds the 8 bit fractional portion of the number.
+	Fraction uint8
+}
+
+// Response16 holds an iCC response16Number.
+type Response16 struct {
+	// Interval holds the interval value
+	Interval uint16
+	// Measurement holds the measurement value.
+	Measurement S15Fixed16
+}
+
+// XYZ number holds a CIE XYZ tristimulus value.
+type XYZNumber struct {
+	X S15Fixed16
+	Y S15Fixed16
+	Z S15Fixed16
+}
+
+// ProfileVersion holds a BCD encoded profile version number.
+type ProfileVersion struct {
+	// Major holds a BCD encoded major version number.
+	Major uint8
+	// Minor holds a BCD encoded minor and patch number. The first digit
+	// is the minor version while the second is the patch level.
+	Minor uint8
+}
+
 // ICC holds an ICC color profile.
 type ICC struct {
 	CMMTypeSignature                 uint32
-	ProfileVersion                   uint32
+	ProfileVersion                   ProfileVersion
 	ProfileClassSignature            uint32
 	ColorSpace                       uint32
 	ProfileConnectionSpace           uint32
@@ -36,9 +85,6 @@ type ICC struct {
 	RenderingIntent                  uint32
 	ProfileConnectionSpaceIlluminant XYZNumber
 	ProfileCreatorSignature          uint32
-}
-
-type XYZNumber struct {
 }
 
 func DecodeICC(ctx context.Context, b []byte, opt ...image.ReadOption) (*ICC, error) {

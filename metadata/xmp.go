f634354f0a41f57bde06f6926061ee5858fec2c1
drswork <dsugalski@google.com>
2020-02-18 22:28:37 -0500

    Add the basic XMP data structure
    Copied more or less verbatim, sorta, from the XMP spec.
    
    XML, for the record, is never the right answer to the question "what data encoding would be good", though it is an excellent answer to the question "does anyone have a reason we should drink heavily and regret our life choices?"

--- metadata/xmp.go
+++ metadata/xmp.go
@@ -3,6 +3,7 @@ package metadata
 import (
 	"context"
 	"errors"
+	"time"
 
 	"github.com/drswork/image"
 )
@@ -19,7 +20,76 @@ func RegisterXMPEncoder(e func(context.Context, *XMP, ...image.WriteOption) (str
 	xmpEncoder = e
 }
 
+type LanguageAlternative struct {
+	Language string
+	Text     string
+}
+
+// Things in the Dublin Core namespace
+type Core struct {
+	Contributor []string
+	Coverage    string
+	Creator     []string
+	Date        []time.Time
+	Description []LanguageAlternative
+	Format      string // this is the mime type
+	Identifier  string
+	Language    []string // Really locales
+	Publisher   []string
+	Relation    []string
+	Rights      []LanguageAlternative
+	Source      string
+	Subject     []string
+	Title       []LanguageAlternative
+	Type        []string
+}
+
+// Things in the XMP namespace
+type XMPSpecific struct {
+	CreateDate   *time.Time
+	CreatorTool  string
+	Identifier   []string
+	Label        string
+	MetadataData *time.Time
+	ModifiedDate *time.Time
+	Rating       float64
+}
+
+// Things in the XMP rights management namespace
+type XMPRights struct {
+	Certificate  string
+	Marked       *bool
+	Owner        []string
+	UsageTerms   []LanguageAlternative
+	WebStatement string
+}
+
+// These three are placeholders and should be fixed later
+type GUID string
+type ResourceRef string
+type RenditionClass string
+
+// Things in the XMP Media Management namespace
+type XMPMediaManagement struct {
+	DerivedFrom        ResourceRef
+	DocumentID         GUID
+	InstanceID         GUID
+	OriginalDocumentID GUID
+	RenditionClass     RenditionClass
+	RenditionParams    string
+}
+
+type XMPIDQ struct {
+	Scheme string
+}
+
+// XMP holds the XMP metadata. It's a collection of sub-types
 type XMP struct {
+	CoreProperties  Core
+	Properties      XMPSpecific
+	Rights          XMPRights
+	MediaManagement XMPMediaManagement
+	IDQ             XMPIDQ
 }
 
 func DecodeXMP(ctx context.Context, b string, opt ...image.ReadOption) (*XMP, error) {
